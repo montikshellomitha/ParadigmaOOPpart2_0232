734bfacc9b5f82c7610bebfbbee662bb49c63880
montikshellomitha <monshelmit@gmail.com>
2025-06-02 19:03:15 +0700

    contoh error: override method perkenalan() yang final di derivedClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -8,3 +8,18 @@ public:
     }
 };
 
+class derivedClass : public baseClass {
+public:
+    void perkenalan()
+    {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
